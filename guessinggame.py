d53109d95aba00fee71bbf773e84a52b3262b9d3
cmzapata96 <cmzapata96@gmail.com>
2020-06-03 17:52:31 -0400

    firrst commit

---
+++ guessinggame.py
@@ -0,0 +1,10 @@
+import random
+
+
+print("-------------------------------")
+print("     M& M Guessing game ")
+print("-------------------------------")
+
+mm_count = random.randint(1, 100)
+
+print(mm_count)
