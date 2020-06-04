71c1634fa8ce944659d8006b2dba66175af71e52
cmzapata96 <cmzapata96@gmail.com>
2020-06-03 22:26:20 -0400

    Enter

--- guessinggame.py
+++ guessinggame.py
@@ -5,6 +5,18 @@ print("-------------------------------")
 print("     M& M Guessing game ")
 print("-------------------------------")
 
-mm_count = random.randint(1, 100)
+print("Guess the number of M&M and you get lunch on the hous!")
+print()
 
-print(mm_count)
+mm_count = random.randint(1, 100)
+attempt_limit = 5
+attempts = 0
+ 
+while attempts < attempt_limit:
+    guess_text = input ("How many M&M arre in the jar?")
+    guess = int(guess_text)
+    print(guess)
+ 
+    attempts += 1
+
+print("Bye")
